2ffddea3d38b4e74bbd61f2c2bd62197b3410624
bealabea <beatricemarin89@yahoo.it>
2022-02-02 18:15:26 +0100

    removes toLowerCase() from :value

--- src/components/SelectApp.vue
+++ src/components/SelectApp.vue
@@ -3,7 +3,7 @@
         <label for="genre" class="px-3">FILTRA PER GENERE</label>
         <select name="genre" id="genre" v-model="selected" @change="$emit('selection', selected)">
             <option value="all">All</option>
-            <option v-for="(disc, index) in genreList" :key="index" :value="disc.genre.toLowerCase()">{{disc.genre}}</option> 
+            <option v-for="(disc, index) in genreList" :key="index" :value="disc.genre">{{disc.genre}}</option> 
         </select>   
     </div>
 </template>
